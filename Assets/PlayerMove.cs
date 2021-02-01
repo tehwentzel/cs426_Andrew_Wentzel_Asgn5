1369bcdc766e155fb0d5f10f06dea13264059ab6
Andrew Wentzel <tehwentzel@gmail.com>
2021-02-01 17:46:51 -0600

    kinda have rotate on collision working and basic game layout

--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -3,15 +3,50 @@ using UnityEngine.Networking;
 
 public class PlayerMove : NetworkBehaviour
 {
+    public float speed = 25.0f;
+    public float rotationSpeed = 90;
+    public float force = 1f;
+    public float invertCameraSpeed = .5f;
+
+    public GameObject playerCamera;
+    public float cameraRotaton = 0f;
+
+    Rigidbody rb;
+    Transform t;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        t = GetComponent<Transform>();
+        playerCamera = gameObject.transform.Find("PlayerCamera").gameObject;
+    }
+
     void Update()
     {
         if (!isLocalPlayer)
             return;
 
-        var x = Input.GetAxis("Horizontal")*0.1f;
-        var z = Input.GetAxis("Vertical")*0.1f;
+        Debug.Log(t.up.y);
+        Debug.Log(Physics.gravity.normalized.y);
+        if(t.up.y != -Physics.gravity.normalized.y & (Mathf.Abs(t.up.y + Physics.gravity.normalized.y) > .001)){
+            t.RotateAround(t.position, this.transform.forward, 1);
+            // t.rotation *= Quaternion.Euler(0,0,180);
+        } else{
+            if (Input.GetKey(KeyCode.D))
+            t.rotation *= Quaternion.Euler(0, rotationSpeed * Time.deltaTime, 0);
+            else if (Input.GetKey(KeyCode.A))
+                t.rotation *= Quaternion.Euler(0, -rotationSpeed * Time.deltaTime, 0);
 
-        transform.Translate(x, 0, z);
+            if (Input.GetKey(KeyCode.W))
+                rb.velocity += this.transform.forward * speed * Time.deltaTime;
+            else if (Input.GetKey(KeyCode.S))
+                rb.velocity -= this.transform.forward * speed * Time.deltaTime;
+
+            // Quaternion returns a rotation that rotates x degrees around the x axis and so on
+
+            if (Input.GetKeyDown(KeyCode.Space))
+                rb.AddForce(t.up * force);
+        }
     }
 
     public override void OnStartLocalPlayer(){
---
+++ Assets/Target.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Target : NetworkBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {    
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player"){
+            Physics.gravity = -Physics.gravity;
+        }
+        //on collision adding point to the score
+    }
+}
